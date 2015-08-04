0b19b73ecb1fc10378f967587ba77ac1db7e25e5
David Clark <david.dave.clark@gmail.com>
2015-08-03 20:14:21 -0700

    Cleanup tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,10 @@
 module.exports = function(config) {
   config.set({
     frameworks: ['tap'],
-    browsers: ['Firefox'],
+    browsers: ['PhantomJS'],
     files: [
+      './node_modules/es5-shim/es5-shim.min.js',
+      './node_modules/es5-shim/es5-sham.min.js',
       './test/test-bundle.js',
     ],
   });
