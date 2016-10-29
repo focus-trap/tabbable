82bf4c5dab680dc2dc6c02ec2c781f56aad49cce
David Clark <dclark@mapbox.com>
2016-10-29 11:38:09 -0700

    Add debug app

---
+++ test/debug.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var path = require('path');
+
+var testCases = {
+  basic: fs.readFileSync(path.join(__dirname, 'fixtures/basic.html'), 'utf8'),
+  changingContent: fs.readFileSync(path.join(__dirname, 'fixtures/changing-content.html'), 'utf8'),
+  jqueryui: fs.readFileSync(path.join(__dirname, 'fixtures/jqueryui.html'), 'utf8'),
+  nested: fs.readFileSync(path.join(__dirname, 'fixtures/nested.html'), 'utf8'),
+  nonLinear: fs.readFileSync(path.join(__dirname, 'fixtures/non-linear.html'), 'utf8'),
+}
+
+var root;
+var content;
+for (var key in testCases) {
+  if (!testCases.hasOwnProperty(key)) continue;
+  root = document.createElement('div');
+  content = '<h2>' + key + '</h2>';
+  content += testCases[key];
+  root.innerHTML = content;
+  document.body.appendChild(root);
+}
+
+document.body.addEventListener('focusin', function(event) {
+  console.log(event.target); // eslint-disable-line no-console
+});
