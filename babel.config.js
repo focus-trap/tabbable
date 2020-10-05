e3da8e90c4fa808b08517b4b8ecff224e85703a0
Stefan Cameron <stefan@stefcameron.com>
2020-10-05 12:55:39 -0500

    Transpile non-minified bundles
    Fixes #96
    
    Also targets ESM browsers for the ESM build, rather than ES5 browsers
    for the other builds.

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,44 @@
 /* eslint-env node */
 
+const plugins = [
+  '@babel/plugin-proposal-nullish-coalescing-operator',
+  '@babel/plugin-proposal-optional-chaining',
+];
+
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: false,
-        loose: true,
-      },
-    ],
-  ],
-  plugins: [
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-optional-chaining',
-  ],
+  env: {
+    es5: {
+      // ES5 browsers for CJS and UMD builds
+      presets: [
+        [
+          // NOTE: With no targets specified, @babel/preset-env will transform all
+          //  ECMAScript 2015+ code by default, which is the original preset prior
+          //  to upgrading to Babel 7
+          // @see https://babeljs.io/docs/en/babel-preset-env#targets
+          '@babel/preset-env',
+          {
+            loose: true,
+          },
+        ],
+      ],
+      plugins,
+    },
+    esm: {
+      // ESM browsers for ESM builds
+      presets: [
+        [
+          // @see https://babeljs.io/docs/en/babel-preset-env#targets
+          '@babel/preset-env',
+          {
+            modules: false, // preserve ES modules
+            loose: true,
+            targets: {
+              esmodules: true,
+            },
+          },
+        ],
+      ],
+      plugins,
+    },
+  },
 };
