4896b7f3b124290486cc2364fc00222e10242306
Ido Rosenthal <idoros@gmail.com>
2021-04-23 13:06:33 +0300

    fix: type of getShadowRoot option

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ type FocusableElement = HTMLElement | SVGElement;
 
 export type CheckOptions = {
   displayCheck?: 'full' | 'non-zero-area' | 'none';
-  getShadowDom?: (node: FocusableElement) => boolean | undefined;
+  getShadowRoot?: (node: FocusableElement) => ShadowRoot | boolean | undefined;
 };
 
 export type TabbableOptions = {
