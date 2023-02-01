07925b7b3fcaef4d8267244b5f7cd4942a805616
Stefan Cameron <stefan@stefcameron.com>
2023-02-01 13:44:46 -0600

    Handle case where root node is itself, indicating detachment

--- .changeset/eight-bulldogs-shake.md
+++ .changeset/eight-bulldogs-shake.md
@@ -2,4 +2,4 @@
 'tabbable': patch
 ---
 
-Fix a corner case (crash) where the `ownerDocument` could be `null` while checking if a node is attached ([focus-trap-react #905](https://github.com/focus-trap/focus-trap-react/issues/905))
+Fix a corner case where a node's root node can be itself, indicating detachment from the DOM, leading to a crash in `isHidden() -> isNodeAttached() -> getRootNode()` if not handled properly ([focus-trap-react #905](https://github.com/focus-trap/focus-trap-react/issues/905))
--- src/index.js
+++ src/index.js
@@ -23,8 +23,8 @@ const matches = NoElement
 
 const getRootNode =
   !NoElement && Element.prototype.getRootNode
-    ? (element) => element.getRootNode()
-    : (element) => element.ownerDocument;
+    ? (element) => element?.getRootNode?.()
+    : (element) => element?.ownerDocument;
 
 /**
  * @param {Element} el container to check in
@@ -276,18 +276,26 @@ const isNodeAttached = function (node) {
   //  if a tabbable/focusable node was quickly added to the DOM, focused, and then removed
   //  from the DOM as in https://github.com/focus-trap/focus-trap-react/issues/905), then
   //  `ownerDocument` will be `null`, hence the optional chaining on it.
-  let nodeRootHost = node && getRootNode(node).host;
-  let attached = !!(
-    nodeRootHost?.ownerDocument?.contains(nodeRootHost) ||
-    node?.ownerDocument?.contains(node)
-  );
+  let nodeRoot = node && getRootNode(node);
+  let nodeRootHost = nodeRoot?.host;
 
-  while (!attached && nodeRootHost) {
-    // since it's not attached and we have a root host, the node MUST be in a nested shadow DOM,
-    //  which means we need to get the host's host and check if that parent host is contained
-    //  in (i.e. attached to) the document
-    nodeRootHost = getRootNode(nodeRootHost).host;
-    attached = !!nodeRootHost?.ownerDocument?.contains(nodeRootHost);
+  // in some cases, a detached node will return itself as the root instead of a document or
+  //  shadow root object, in which case, we shouldn't try to look further up the host chain
+  let attached = false;
+  if (nodeRoot && nodeRoot !== node) {
+    attached = !!(
+      nodeRootHost?.ownerDocument?.contains(nodeRootHost) ||
+      node?.ownerDocument?.contains(node)
+    );
+
+    while (!attached && nodeRootHost) {
+      // since it's not attached and we have a root host, the node MUST be in a nested shadow DOM,
+      //  which means we need to get the host's host and check if that parent host is contained
+      //  in (i.e. attached to) the document
+      nodeRoot = getRootNode(nodeRootHost);
+      nodeRootHost = nodeRoot?.host;
+      attached = !!nodeRootHost?.ownerDocument?.contains(nodeRootHost);
+    }
   }
 
   return attached;
